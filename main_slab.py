2c686d23d029ce89c4f1432387674a9058ca4b84
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2021-11-15 18:24:18 -0500

    Remove my DB Credentials

--- main_slab.py
+++ main_slab.py
@@ -11,8 +11,8 @@ cat_flows = CatFlows(cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)])
 # Get Launchpad
 launchpad = cat_flows.submit(
     hostname="localhost",
-    db_name="mo_stability",
-    port=27017,
-    username="mo_stability_admin",
-    password="ww1www3w3ree22s223eew",
+    db_name="<<DB-NAME>>",
+    port="<<DB-PORT>>",
+    username="<<DB-USERNAME>>",
+    password="<<DB-PASSWORD>>",
 )
