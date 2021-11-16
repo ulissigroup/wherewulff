5754660bc478220a9f22b073e9ce1fd3e4af7c6f
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2021-11-16 16:45:53 -0500

    Add oh_clockwise_rotation function

--- CatFlows/workflows/surface_pourbaix.py
+++ CatFlows/workflows/surface_pourbaix.py
@@ -1,13 +1,75 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import uuid
+import numpy as np
 
-from fireworks import Firework, Workflow
+from fireworks import Workflow
 from atomate.vasp.config import VASP_CMD, DB_FILE
 
-from CatFlows.firetasks.slab_ads import get_clockwise_rotations
 from CatFlows.fireworks.optimize import AdsSlab_FW
 from CatFlows.fireworks.surface_pourbaix import SurfacePBX_FW
 from CatFlows.adsorption.MXide_adsorption import MXideAdsorbateGenerator
+
+
+# Angles list
+def get_angles(n_rotations=4):
+    """Get angles like in the past"""
+    angles = []
+    for i in range(n_rotations):
+        deg = (2 * np.pi / n_rotations) * i
+        angles.append(deg)
+    return angles
+
+
+def add_adsorbates(adslab, ads_coords, molecule):
+    """Add molecule in all ads_coords once"""
+    translated_molecule = molecule.copy()
+    for ads_site in ads_coords:
+        for mol_site in translated_molecule:
+            new_coord = ads_site + mol_site.coords
+            adslab.append(
+                mol_site.specie,
+                new_coord,
+                coords_are_cartesian=True,
+                properties=mol_site.properties,
+            )
+    return adslab
+
+
+# Try the clockwise thing again...
+def get_clockwise_rotations(slab, molecule):
+    """We need to rush function..."""
+    # This will be a inner method
+    mxidegen = MXideAdsorbateGenerator(
+        slab, repeat=[1, 1, 1], verbose=False, positions=["MX_adsites"], relax_tol=0.025
+    )
+    bulk_like_sites, _ = mxidegen.get_bulk_like_adsites()
+
+    # set n_rotations to 1 if mono-atomic
+    n = len(molecule[0]) if type(molecule).__name__ == "list" else len(molecule)
+    n_rotations = 1 if n == 1 else 4
+
+    # Angles
+    angles = get_angles(n_rotations=n_rotations)
+
+    # Molecule formula
+    molecule_comp = molecule.composition.as_dict()
+    molecule_formula = "".join(molecule_comp.keys())
+
+    # rotate OH
+    molecule_rotations = mxidegen.get_transformed_molecule_MXides(
+        molecule, axis=[0, 0, 1], angles_list=angles
+    )
+
+    # placement
+    adslab_dict = {}
+    for rot_idx in range(len(molecule_rotations)):
+        slab_ads = slab.copy()
+        slab_ads = add_adsorbates(
+            slab_ads, bulk_like_sites, molecule_rotations[rot_idx]
+        )
+        adslab_dict.update({"{}_{}".format(molecule_formula, rot_idx + 1): slab_ads})
+
+    return adslab_dict
 
 
 def SurfacePBX_WF(slab, slab_uuid, oriented_uuid, adsorbates, vasp_cmd=VASP_CMD, db_dile=DB_FILE):
