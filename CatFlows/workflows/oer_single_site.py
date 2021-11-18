a473b2980d0e777fe2c5ffa819816490d784baba
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2021-11-18 18:51:11 -0500

    OERSingleSite Workflow function

---
+++ CatFlows/workflows/oer_single_site.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import uuid
+import numpy as np
+
+from fireworks import Workflow
+from atomate.vasp.config import VASP_CMD, DB_FILE
+
+from pymatgen.core.surface import Slab
+
+from CatFlows.fireworks.optimize import AdsSlab_FW
+
+
+
+def OERSingleSite_WF(oer_dict, slab, slab_uuid, oriented_uuid, vasp_cmd=VASP_CMD, db_file=DB_FILE):
+    """
+    Wrap-up workflow for OER single site (wna) + Reactivity Analysis
+
+    Args:
+
+    Returns:
+        something
+    """
+    # Empty lists
+    oer_fws, oer_uuids = [], []
+
+    # Reduced formula
+    general_reduced_formula = slab.composition.reduced_formula
+    miller_index = "".join(list(map(str, slab.miller_index)))
+
+
+    # Loop over OER intermediates
+    for oer_inter_label, oer_inter in oer_dict.items():
+        oer_intermediate = Slab.from_dict(oer_inter)
+        reduced_formula = oer_intermediate.composition.reduced_formula
+        name = (f"{reduced_formula}-{miller_index}-{oer_inter_label}")
+        oer_inter_uuid = uuid.uuid4()
+        oer_inter_fw = AdsSlab_FW(
+            oer_intermediate,
+            name=name,
+            oriented_uuid=oriented_uuid,
+            slab_uuid=slab_uuid,
+            ads_slab_uuid=oer_inter_uuid,
+            vasp_cmd=vasp_cmd
+            )
+        oer_fws.append(oer_inter_fw)
+        oer_uuids.append(oer_inter_uuid)
+
+    # Reactivity Analysis
+
+
+    # Create the workflow
+    all_fws = oer_fws
+    oer_single_site = Workflow(
+        all_fws,
+        name=f"{general_reduced_formula}-{miller_index}-OER SingleSite"
+    )
+    return oer_single_site
