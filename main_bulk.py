4de80c56e7d60f9228a106cfa35dfce2fb503b4a
Ian Benlolo <ian.benlolo@gmail.com>
2021-11-22 12:24:07 -0800

    first push

--- main_bulk.py
+++ main_bulk.py
@@ -3,7 +3,7 @@ from CatFlows.launchers.bulkflows import BulkFlows
 from fireworks.core.rocket_launcher import rapidfire
 
 # Import CIF file
-cif_file = "./RuO2_136.cif"
+cif_file = "/global/homes/i/ibenlo/RuTiCrOx_bulks/Ti1Cr1Ru2O8_1.cif"
 
 # BulkFlow method and config
 bulk_flow = BulkFlows(cif_file)
@@ -11,8 +11,8 @@ bulk_flow = BulkFlows(cif_file)
 # Get Launchpad
 launchpad = bulk_flow.submit(
     hostname="localhost",
-    db_name="<<DB-NAME>>",
-    port="<<DB-PORT>>",
-    username="<<DB-USERNAME>>",
-    password="<<DB-PASSWORD>>",
+    db_name="ib_mongo",
+    port="27017",
+    username="ib_mongo_admin",
+    password="mypass",
 )
