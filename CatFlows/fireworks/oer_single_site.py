7f460a65a262ed515ed4bb41c52319bc7c156416
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2021-12-06 12:18:49 -0500

    Adding convert function from FireTask to FireWorks

---
+++ CatFlows/fireworks/oer_single_site.py
@@ -0,0 +1,41 @@
+from pymatgen.core import surface
+from fireworks import Firework
+
+from atomate.vasp.fireworks.core import OptimizeFW
+from atomate.vasp.config import VASP_CMD, DB_FILE
+from atomate.utils.utils import get_meta_from_structure
+
+from CatFlows.analysis.oer import OER_SingleSiteAnalyzer
+
+
+def OER_SingleSiteAnalyzer_FW(
+    reduced_formula="",
+    name="",
+    miller_index="",
+    slab_uuid="",
+    ads_slab_uuids="",
+    surface_termination="",
+    parents=None,
+    db_file=DB_FILE,
+):
+    """
+    Converts the OER_SingleSiteAnalyzer FireTask to FireWorks.
+    """
+
+    # FW
+    fw = Firework(
+        OER_SingleSiteAnalyzer(
+            reduced_formula=reduced_formula,
+            miller_index=miller_index,
+            slab_uuid=slab_uuid,
+            ads_slab_uuids=ads_slab_uuids,
+            surface_termination=surface_termination,
+            db_file=db_file,
+            to_db=True,
+        ),
+        name=name,
+        parents=parents,
+        
+    )
+
+    return fw
