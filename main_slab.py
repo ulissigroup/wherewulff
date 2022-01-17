9dcaa094cb5317693e3654a426fe7b29fc348325
Yuri <rsanspeu@andrew.cmu.edu>
2022-01-17 15:42:47 -0500

    By default run_fake should be set to False.

--- main_slab.py
+++ main_slab.py
@@ -6,11 +6,14 @@ from fireworks.core.rocket_launcher import rapidfire
 cif_file = "./RuO2_136.cif"
 
 # CatFlows method and config
-cat_flows = CatFlows(cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)], run_fake=True)
+cat_flows = CatFlows(
+    cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)], run_fake=False
+)
 
 # Get Launchpad
 launchpad = cat_flows.submit(
     db_name="<<DB-NAME>>",
     port="<<DB-PORT>>",
     username="<<DB-USERNAME>>",
-    password="<<DB-PASSWORD>>",)
+    password="<<DB-PASSWORD>>",
+)
