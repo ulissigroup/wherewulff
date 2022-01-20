c2034aa32ad635294f8772adc631e7327513bc79
Yuri <rsanspeu@andrew.cmu.edu>
2022-01-20 16:58:21 -0500

    Add the script that will run the end to end workflow for unit test purposes.

--- main_slab.py
+++ main_slab.py
@@ -3,17 +3,21 @@ from CatFlows.launchers.catflows import CatFlows
 from fireworks.core.rocket_launcher import rapidfire
 
 # Import CIF file
-cif_file = "./RuO2_136.cif"
+cif_file = "./benchmark/mp_oxide_cifs/IrO2_136.cif"
 
 # CatFlows method and config
 cat_flows = CatFlows(
-    cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)], run_fake=False
+    cif_file,
+    exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)],
+    run_fake=True,
+    metal_site="Ir",
 )
 
 # Get Launchpad
 launchpad = cat_flows.submit(
-    db_name="<<DB-NAME>>",
-    port="<<DB-PORT>>",
-    username="<<DB-USERNAME>>",
-    password="<<DB-PASSWORD>>",
+    hostname="localhost",
+    db_name="fireworks",
+    port=27017,
+    username="",
+    password="",
 )
