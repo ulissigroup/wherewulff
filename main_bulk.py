1769181bf815c54ff1906696ba7dd0a56b55614a
Ian Benlolo <ian.benlolo@gmail.com>
2022-02-02 12:23:03 -0800

    hiding credentials

--- main_bulk.py
+++ main_bulk.py
@@ -1,6 +1,10 @@
 from CatFlows.launchers.bulkflows import BulkFlows
 
 from fireworks.core.rocket_launcher import rapidfire
+import yaml
+
+# load yml file to dictionary
+credentials = yaml.load(open('./credentials.yml'))['db']
 
 # Import CIF file
 cif_file = "/global/homes/i/ibenlo/RuTiCrOx_bulks/Ti1Cr1Ru2O8_1.cif"
@@ -10,9 +14,9 @@ bulk_flow = BulkFlows(cif_file)
 
 # Get Launchpad
 launchpad = bulk_flow.submit(
-    hostname="localhost",
-    db_name="ib_mongo",
-    port="27017",
-    username="ib_mongo_admin",
-    password="mypass",
+    hostname=credentials['hostname'],
+    db_name=credentials['db_name'],
+    port=credentials['port'],
+    username=credentials['username'],
+    password=credentials['password'],
 )
