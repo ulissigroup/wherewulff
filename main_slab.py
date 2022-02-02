482f7475fdb936aced8430155955e43b5b12e7d6
Ian Benlolo <ian.benlolo@gmail.com>
2022-02-02 12:42:40 -0800

    More hiding credentials

--- main_slab.py
+++ main_slab.py
@@ -1,6 +1,8 @@
 from CatFlows.launchers.catflows import CatFlows
 from fireworks.core.rocket_launcher import rapidfire
 
+import yaml
+credentials = yaml.load(open('./credentials.yml'))['db']
 
 # Import CIF file
 cif_file = "/global/cscratch1/sd/ibenlo/mongo_results/block_2021-12-19-20-42-33-847568/launcher_2022-01-09-04-05-08-234423/slab.cif"
@@ -10,9 +12,9 @@ cat_flows = CatFlows(cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)])
 
 # Get Launchpad
 launchpad = cat_flows.submit(
-    hostname="mongodb05.nersc.gov",
-    db_name="ib_mongo",
-    port=27017,
-    username="ib_mongo_admin",
-    password="m0ngou0mo12",
+    hostname=credentials['hostname'],
+    db_name=credentials['db_name'],
+    port=credentials['port'],
+    username=credentials['username'],
+    password=credentials['password'],
 )
