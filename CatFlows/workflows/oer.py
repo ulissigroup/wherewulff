2250443f40ecac7d6eb5acd4b9d73892b42a6400
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2022-02-14 17:23:26 -0500

    Adding input structures, surface pbx uuid, clean-up

--- CatFlows/workflows/oer.py
+++ CatFlows/workflows/oer.py
@@ -9,12 +9,16 @@ from CatFlows.firetasks.oer_single_site import OERSingleSiteFireTask
 def OER_WF(
     bulk_structure,
     miller_index,
+    slab_orig,
+    bulk_like_sites,
+    ads_dict_orig,
     metal_site,
     applied_potential=1.60,
     applied_pH=0,
     parents=None,
     vasp_cmd=VASP_CMD,
     db_file=DB_FILE,
+    surface_pbx_uuid="",
 ):
     """
     Wrap-up workflow to do the OER Single site WNA after SurfacePBX.
@@ -32,67 +36,24 @@ def OER_WF(
     # Bulk structure formula
     bulk_formula = bulk_structure.composition.reduced_formula
 
-    # Filter out fw that are not same miller_index
-    #    parents_hkl = [fw for fw in parents if f"PBX-{bulk_formula}-{miller_index}" in fw.name]
-
     # WulffShape Analysis
     oer_fw = Firework(
         OERSingleSiteFireTask(
             reduced_formula=bulk_formula,
             miller_index=miller_index,
+            slab_orig=slab_orig,
+            bulk_like_sites=bulk_like_sites,
+            ads_dict_orig=ads_dict_orig,
             metal_site=metal_site,
             applied_potential=applied_potential,
             applied_pH=applied_pH,
             db_file=db_file,
             vasp_cmd=vasp_cmd,
+            surface_pbx_uuid=surface_pbx_uuid,
         ),
         name=f"{bulk_formula}-{miller_index} OER Single Site WNA",
         parents=parents,
     )
 
-    #    all_fws = [oer_fw]
-    #    if parents is not None:
-    #        all_fws.extend(parents)
-    #    oer_wf = Workflow(all_fws, name=f"{bulk_formula}-{miller_index} OER Single Site WNA")
     return oer_fw
 
-
-def OER_WF_new(
-    bulk_structure, miller_index_list, parents=None, vasp_cmd=VASP_CMD, db_file=DB_FILE
-):
-    """
-    Wrap-up workflow to do the OER Single site WNA after SurfacePBX.
-    Args:
-        bulk_structure (Structure): Bulk structure to refer the wulff shape
-        vasp_cmd: vasp executable
-        db_file: database file.
-    Returns:
-        JSON file with Wulff Analysis.
-    """
-    # Bulk structure formula
-    bulk_formula = bulk_structure.composition.reduced_formula
-
-    # PBX parents
-    parents_pbx = [fw for fw in parents if f"PBX-{bulk_formula} in fw.name"]
-
-    # Loop over hkl
-    all_fws = []
-    for hkl in miller_index_list:
-        exclude_pbx_parents = [fw for fw in parents_pbx if f"{hkl}" not in fw.name]
-        parents_hkl = list(set(parents) - set(exclude_pbx_parents))
-        oer_fw = Firework(
-            OERSingleSiteFireTask(
-                reduced_formula=bulk_formula,
-                miller_index=hkl,
-                db_file=db_file,
-                vasp_cmd=vasp_cmd,
-            ),
-            name=f"{bulk_formula}-{hkl} OER Single Site WNA",
-            parents=parents_hkl,
-        )
-        all_fws.append(oer_fw)
-
-    if parents is not None:
-        all_fws.extend(parents)
-        oer_wf = Workflow(all_fws, name=f"{bulk_formula} OER Single Site WNA")
-    return oer_wf
