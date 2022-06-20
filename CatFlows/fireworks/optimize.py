685bfdb9952cab3b41ba56e784d5c0c33589039d
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2022-06-20 17:36:05 -0400

    adding LICENSE header

--- CatFlows/fireworks/optimize.py
+++ CatFlows/fireworks/optimize.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2022 Carnegie Mellon University.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 from typing import Counter
 from atomate.vasp.fireworks.core import OptimizeFW
 from atomate.vasp.config import VASP_CMD, DB_FILE
