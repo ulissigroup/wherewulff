efcda7554386b517de52f514ccd133207fe45d97
Javier Heras-Domingo <jherasdo@andrew.cmu.edu>
2022-06-21 13:39:34 -0400

    Removing old atomate_run.sh bash script

--- atomate_run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#atomate_run.sh for workflows in python scripts.
-
-#Variables
-workflow=main_bulk_local.py
-
-#Add structure and workflow
-mongod &>/dev/null & python $workflow
