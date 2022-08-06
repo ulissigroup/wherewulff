979bb27b6e0980e3cd7ffde0814094e4e81eedcb
Yuri <rsanspeu@andrew.cmu.edu>
2022-08-06 18:02:52 -0400

    Adding more analysis for the results from hubbard u sensitivity.

--- u_effect.py
+++ u_effect.py
@@ -2,6 +2,7 @@ from fireworks import FiretaskBase, FWAction, explicit_serialize
 from atomate.vasp.database import VaspCalcDb
 from atomate.vasp.config import VASP_CMD, DB_FILE
 from atomate.utils.utils import env_chk
+from pymatgen.core import Structure
 import numpy as np
 import pandas as pd
 
@@ -23,8 +24,6 @@ class analyzeUEffect(FiretaskBase):
         # things have converged
         results_dict = {}
         for k, s_ads_uuid in zip(keys, slab_adslab_uuids):
-            #        breakpoint()
-            print(k)
             slab_uuid = s_ads_uuid["slab_uuid"]
             adslab_uuid = s_ads_uuid["adslab_uuid"]
             has_slab_converged = mmdb.db["tasks"].find_one({"uuid": slab_uuid})[
@@ -51,14 +50,35 @@ class analyzeUEffect(FiretaskBase):
             slab_energy = mmdb.db["tasks"].find_one({"uuid": slab_uuid})[
                 "calcs_reversed"
             ][0]["output"]["energy"]
+            slab_bandgap = mmdb.db["tasks"].find_one({"uuid": slab_uuid})[
+                "calcs_reversed"
+            ][0]["output"]["bandgap"]
             adslab_energy = mmdb.db["tasks"].find_one({"uuid": adslab_uuid})[
                 "calcs_reversed"
             ][0]["output"]["energy"]
-            if U_value in results_dict:
+            contcar = mmdb.db["tasks"].find_one({"uuid": adslab_uuid})[
+                "calcs_reversed"
+            ][0]["output"]["structure"]
+            magmoms = [
+                x["tot"]
+                for x in mmdb.db["tasks"].find_one({"uuid": adslab_uuid})[
+                    "calcs_reversed"
+                ][0]["output"]["outcar"]["magnetization"]
+            ]
+            adslab_bandgap = mmdb.db["tasks"].find_one({"uuid": adslab_uuid})[
+                "calcs_reversed"
+            ][0]["output"]["bandgap"]
+            if U_value in results_dict:  # update with the lowest energy config
                 if adslab_energy < results_dict[U_value]["adslab_energy"]:
                     results_dict[U_value]["adslab_energy"] = adslab_energy
                     results_dict[U_value]["has_adslab_converged"] = has_adslab_converged
                     results_dict[U_value]["adslab_uuid"] = adslab_uuid
+                    results_dict[U_value]["configuration"] = k.split("_")[1]
+                    results_dict[U_value]["contcar"] = contcar
+                    results_dict[U_value]["magmoms"] = magmoms
+                    results_dict[U_value]["slab_bandgap"] = slab_bandgap
+                    results_dict[U_value]["adslab_bandgap"] = adslab_bandgap
+
             else:
                 results_dict[U_value] = {
                     "slab_energy": slab_energy,
@@ -67,11 +87,23 @@ class analyzeUEffect(FiretaskBase):
                     "slab_uuid": slab_uuid,
                     "adslab_uuid": adslab_uuid,
                     "has_adslab_converged": has_adslab_converged,
+                    "configuration": k.split("_")[1],
+                    "contcar": contcar,
+                    "magmoms": magmoms,
+                    "slab_bandgap": slab_bandgap,
+                    "adslab_bandgap": adslab_bandgap,
                 }
+        # Here we need to go over the adslab contcars and export them to disk for ase gui
+        for U in results_dict:
+            config = results_dict[U]["configuration"]
+            Structure.from_dict(results_dict[U]["contcar"]).to(
+                filename=f"POSCAR_adslab_{config}_{U}"
+            )
+
         # Create pandas dict
         df = pd.DataFrame.from_dict(results_dict, orient="index")
         df_sort = df.sort_index()
-        df_sort.to_csv("sensitivity_U_ads_energy.csv")
+        df_sort.to_csv("sensitivity_U_ads_energy_new.csv")
 
         # Access the tasks collection
         #        print(f"{fw_spec}")
