873990caccd52c1de4856a6ba1c8ffcb3eb84f1d
Yuri <rsanspeu@andrew.cmu.edu>
2022-08-29 12:32:01 -0400

    Making the changes to the chemical potential based on the alkaline condition, which switched the chemical potential of O.

--- CatFlows/firetasks/surface_energy.py
+++ CatFlows/firetasks/surface_energy.py
@@ -16,16 +16,20 @@ from atomate.vasp.database import VaspCalcDb
 
 logger = get_logger(__name__)
 METAL_BULK_ENERGIES = {
-    "Ti": -18.17,  # obtained from bulk Ti2O4 - 4*chempot(O)
+    #    "Ti": -18.17,  # obtained from bulk Ti2O4 - 4*chempot(O)
+    "Ti": -16.69,
     "Cr": -9.6530,
     "Ru": -9.2744,
     #    "O": -7.48175514 - 0.27,
-    "O": -4.28,  # taken from Theoretical Investigation of the Activity of Cobalt Oxides for the
+    #    "O": -4.28,  # taken from Theoretical Investigation of the Activity of Cobalt Oxides for the
+    "O": -5.02,
     # Electrochemical Oxidation of Water
     "Ba": -1.9190,
     "Sr": -1.6831,
     #    "Co": -7.0922,
-    "Co": -8.48,  # Corrected for +U and DFT settings
+    #    "Co": -8.48,  # Corrected for +U and DFT settings
+    #    "Co": -7.00,
+    "Co": -6.258,
     "Sn": -3.9923,
 }  # ev/Atom from MP?
 
