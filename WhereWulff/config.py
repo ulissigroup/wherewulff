791d3f9a7fb7cb5d4c080f22ad1dae6b0587ddbf
jherasdo <jherasdo@andrew.cmu.edu>
2022-09-29 11:49:37 -0400

    adding config file with abstracted variables for the model

---
+++ WhereWulff/config.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2022 Carnegie Mellon University.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+# Include this fields in my_fworker.yml file
+MODEL_CHECKPOINT = ">>model_checkpoint<<"
+MODEL_CONFIG = ">>model_config<<"
+MODEL_DEVICE = ">>model_device<<"
