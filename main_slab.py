f0fedd4d602b44e661423629b01fa704aa3baf0d
Yuri <rsanspeu@andrew.cmu.edu>
2023-03-13 14:15:23 -0400

    Pointing the main_slab.py script to the BaSrCoO cif file and running only for 001 facet.

--- main_slab.py
+++ main_slab.py
@@ -9,10 +9,12 @@ from WhereWulff.launchers.slabflows import SlabFlows
 
 
 # Import CIF file
-cif_file = "<<YOUR_CIF_FILE>>"
+cif_file = "./BaSrCo2O6_bulk.cif"
 
 # CatFlows method and config
-cat_flows = SlabFlows(cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (0, 0, 1)])
+cat_flows = SlabFlows(
+    cif_file, exclude_hkl=[(1, 0, 0), (1, 1, 1), (1, 1, 0), (1, 0, 1)]
+)  # Only do 001 for this example
 
 # Get Launchpad
 launchpad = cat_flows.submit(
