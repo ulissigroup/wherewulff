cd7657792f33330792ea01d53a78fedc38c48ac7
Yuri <rsanspeu@andrew.cmu.edu>
2023-03-13 16:00:54 -0400

    Removing the input/output folder from main branch since it is on a separate branch.

--- BaSrCoO_001_results/copy_vasp_files.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:840d81980169e147f0d7144d4d339aa073a2c32a19fa423ff16aa6f4c9016029
-size 352
--- BaSrCoO_001_results/get_energies.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2276272752d6c40e9df09f9fb0182bc44f9546e4b8a5b6df4cc5211c1cdf886d
-size 290
