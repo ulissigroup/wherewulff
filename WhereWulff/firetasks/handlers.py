d7bde4b604b44ba73bf0b56978c9568d8f6ccbd2
Yuri <rsanspeu@andrew.cmu.edu>
2023-07-12 14:28:05 -0400

    Making the charge range larger for Au (100).

--- WhereWulff/firetasks/handlers.py
+++ WhereWulff/firetasks/handlers.py
@@ -67,7 +67,7 @@ class ContinueOptimizeFW(FiretaskBase):
         parents = []
         all_fws = []
         uuids = []
-        for nelect in np.arange(nelect_orig - 0.3, nelect_orig + 0.31, 0.1):  # SP FWs
+        for nelect in np.arange(nelect_orig - 2.41, nelect_orig + 2.41, 0.8):  # SP FWs
             new_uuid = uuid.uuid4()
             uuids.append(new_uuid)
             incar_dict["user_incar_settings"].update({"NELECT": nelect})
